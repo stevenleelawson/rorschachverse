9d200a4b91415c54b48bbeb57c33b69cc14d0d80
Steven Lawson <unluckygamblermusic@gmail.com>
2016-07-29 13:33:37 -0600

    updating db

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,21 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    development: {
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+     client: 'pg',
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+        connection: 'postgres://localhost/rorschachverse'
+
+     },
+
+     production:{
+
+        client: 'pg',
+
+        connection: process.env.DATABASE_URL + '?ssl=true'
+
     }
-  }
 
 };
